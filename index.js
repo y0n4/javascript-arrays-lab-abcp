39cbcb3c0d7bfff7526e3c1fb61384c45ecb58eb
yona l <04l.yona@gmail.com>
2018-01-18 07:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrepandKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
